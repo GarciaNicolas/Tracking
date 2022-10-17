7a803c5e16cf1af992a2861facd42d7f215a4878
Nicolás <nicolas.f.garcia03@gmail.com>
2022-10-17 09:55:32 -0300

    Ya usable

--- tracking.py
+++ tracking.py
@@ -1,12 +1,7 @@
 '''
 To Do:
--Que se pueda hacer mas de una pasada al video (Guardando la visualizacion de los trackers)
+-Guardar punto central de trackers durante el loop
 -Intentar hacer reconocimiento por contornos en los otros videos
-
-
--Cambiar como se guardan los archivos
-
-
 
 '''
 
@@ -21,13 +16,15 @@ def reescalarImagen (imagen, porcentajeDeEscalado):
     imagen = cv.resize(imagen, dimension, interpolation= cv.INTER_AREA)
     return imagen
 
-def estaDentro(inicioRectangulo, finRectangulo, dimensionImagen):
+def estaDentro(inicioRectangulo, finRectangulo, anchoFrame, largoFrame):
    # True si el rectangulo esta dentro de la imagen y además no toca ningún borde
-    return ((0 < inicioRectangulo[0]< dimensionImagen[1]) and (0 < inicioRectangulo[1]< dimensionImagen[0])) and ((0 < finRectangulo[0]< dimensionImagen[1]) and (0 < finRectangulo[1]< dimensionImagen[0]))
+    return ((0 < inicioRectangulo[0]< largoFrame) and (0 < inicioRectangulo[1]< anchoFrame)) and ((0 < finRectangulo[0]< largoFrame) and (0 < finRectangulo[1]< anchoFrame))
 
-def pop_all(l):
-    r, l[:] = l[:], []
-    return r
+#def pop_all(l):
+#    r, l[:] = l[:], []
+#    return r
+
+
 
 ap = argparse.ArgumentParser()
 ap.add_argument('-d','--dirVideo', required=True, help='Directorio del video')
@@ -42,21 +39,21 @@ identificador = 0
 flagPausa = False
 datosRecolectados = open(args['nombreArchivo']+'.txt','w')
 
-while video.isOpened():
+largoFrame = int(video.get(3))
+anchoFrame = int(video.get(4))
+datosRecolectados.write(str(largoFrame) + ' '+ str(anchoFrame) + ' ')
+
+
+
+while True:
     success, frame = video.read()
     
     #frame = reescalarImagen(frame, 85)
     
-    #datosRecolectados.write(dimensionDeImagen)
-    
     if success:
-        dimensionDeImagen = frame.shape
         frameNumero += 1
-        #if frameNumero == video.get(cv.CAP_PROP_FRAME_COUNT):
-         #   frame_counter = 0 #Or whatever as long as it is the same as next line
-          #  video.set(cv.CAP_PROP_POS_FRAMES, 0)
-        
         primerTecla = cv.waitKey(1) & 0xff
+
         #-Seleccion de Trackers-------------
         if primerTecla == ord(" ") or flagPausa: # Espacio para pausar
             zonasDeInteres = cv.selectROIs("MultiTracker", frameConObjetos)         
@@ -76,30 +73,38 @@ while video.isOpened():
             _, caja = tracker.update(frame)
             inicioRectangulo = (int(caja[0]), int(caja[1]))
             finRectangulo = (int(caja[0] + caja[2]), int(caja[1] + caja[3]))
-            if estaDentro(inicioRectangulo, finRectangulo, dimensionDeImagen):
+            if estaDentro(inicioRectangulo, finRectangulo, anchoFrame, largoFrame):
+                
                 cv.rectangle(frame, inicioRectangulo, finRectangulo, color, 2, 1)
                 puntoMedioX = (finRectangulo[0] + inicioRectangulo[0])//2
                 puntoMedioY = (finRectangulo[1] + inicioRectangulo[1])//2
+                #cv.circle(frame,(puntoMedioX,puntoMedioY), 3, (0,255,0), -1 )
                 datos = str(frameNumero) + ' '+ str(identificador) + ' ' + str(puntoMedioX) + ' ' + str(puntoMedioY) + ' '
+                
+
                 datosRecolectados.write(datos)
+
             else:
                 informacionTrackers.pop(i)
         #--------------------------------------------------------------------
         frameConObjetos = frame.copy() # Para mostrar los rectángulos ya trackeados en la selección de nuevos rectángulos
+        
+
+
         cv.imshow("MultiTracker", frame)
         teclaDeControl = cv.waitKey(1) & 0xff 
         if teclaDeControl == ord("q"): # presionar q para salir del programa
             break
         elif teclaDeControl == ord(" "): # presionar Espacio para salir del programa
             flagPausa = True
+    
     else:
-        pop_all(informacionTrackers)
-        video.set(cv.CAP_PROP_POS_FRAMES, 0)
-        continue
+        break
+        #frameNumero = 0
+        #pop_all(informacionTrackers)
+        #video.set(cv.CAP_PROP_POS_FRAMES, 0)
+        #continue
           
-frameNumero = 0
-
-#datosRecolectados.write(' DimIm', dimensionDeImagen)
         
 datosRecolectados.close()
 video.release()
